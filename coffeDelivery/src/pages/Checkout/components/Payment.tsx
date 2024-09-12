0d252b2d3ed6be70d526d53cb9dbaa254edc2115
hugo vilar <hugovilar04@gmail.com>
2024-09-12 08:25:08 -0300

    FEAT (payment) - adding styles for the selected button

--- coffeDelivery/src/pages/Checkout/components/Payment.tsx
+++ coffeDelivery/src/pages/Checkout/components/Payment.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { CircleDollarSign, CreditCard, DollarSign } from "lucide-react";
 
 export function Payment() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (button: any) => {
+    setSelectedButton(button);
+  };
+
   return (
     <div className="bg-coffe-800 p-4 ml-[340px]">
       <div className="flex">
@@ -13,20 +20,66 @@ export function Payment() {
         </div>
       </div>
       <div className="flex items-start mt-6">
-        <button className="bg-coffe-901 p-7 h-[38px] flex justify-start items-center gap-1 rounded-md ml-5">
-          <CreditCard className="text-coffe-300 size-5" />
-          <p className="whitespace-nowrap text-sm">CARTÃO DE CRÉDITO</p>
+        <button
+          onClick={() => handleButtonClick("credit")}
+          className={`p-7 h-[38px] flex justify-start items-center gap-1 rounded-md ml-5 ${
+            selectedButton === "credit"
+              ? "bg-purple-100 border border-purple-600"
+              : "bg-gray-200"
+          }`}>
+          <CreditCard
+            className={`size-5 ${
+              selectedButton === "credit" ? "text-purple-600" : "text-coffe-300"
+            }`}
+          />
+          <p
+            className={`whitespace-nowrap text-sm ${
+              selectedButton === "credit" ? "text-purple-600" : "text-coffe-700"
+            }`}>
+            CARTÃO DE CRÉDITO
+          </p>
         </button>
-        <button className="bg-coffe-901 p-7 h-[38px] flex justify-center items-center gap-1 rounded-md ml-5">
-          <CreditCard className="text-coffe-300 size-5" />
-          <p className="whitespace-nowrap text-sm">CARTÃO DE DÉBITO</p>
+
+        <button
+          onClick={() => handleButtonClick("debit")}
+          className={`p-7 h-[38px] flex justify-center items-center gap-1 rounded-md ml-5 ${
+            selectedButton === "debit"
+              ? "bg-purple-100 border border-purple-600"
+              : "bg-gray-200"
+          }`}>
+          <CreditCard
+            className={`size-5 ${
+              selectedButton === "debit" ? "text-purple-600" : "text-coffe-300"
+            }`}
+          />
+          <p
+            className={`whitespace-nowrap text-sm ${
+              selectedButton === "debit" ? "text-purple-600" : "text-coffe-700"
+            }`}>
+            CARTÃO DE DÉBITO
+          </p>
         </button>
-        <button className="bg-coffe-901 p-7 h-[38px] flex justify-center items-center gap-1 rounded-md ml-5">
-          <CircleDollarSign className="text-coffe-300 size-5" />
-          <p className="text-sm">DINHEIRO</p>
+
+        <button
+          onClick={() => handleButtonClick("cash")}
+          className={`p-7 h-[38px] flex justify-center items-center gap-1 rounded-md ml-5 ${
+            selectedButton === "cash"
+              ? "bg-purple-100 border border-purple-600"
+              : "bg-gray-200"
+          }`}>
+          <CircleDollarSign
+            className={`size-5 ${
+              selectedButton === "cash" ? "text-purple-600" : "text-coffe-300"
+            }`}
+          />
+          <p
+            className={`whitespace-nowrap text-sm ${
+              selectedButton === "cash" ? "text-purple-600" : "text-coffe-700"
+            }`}>
+            DINHEIRO
+          </p>
         </button>
       </div>
     </div>
   );
 }
-
